73b78fff3f6b81a84e6fcc0ea06d223ab3f7e2cc
alisdair sullivan <alisdair.sullivan@unity3d.com>
2022-10-25 10:22:22 -0700

    last test

--- Scripts/pre.sh
+++ Scripts/pre.sh
@@ -3,7 +3,7 @@
 echo "running pre-build steps"
 echo $(node --version)
 
-echo $(git ls-remote ssh://git@nas.mirrorscapexr.com/mirrorscape/planeshifter.git)
+echo $(git ls-remote ssh://git@nas.mirrorscapexr.com:mirrorscape/planeshifter.git)
 echo $(curl nas.mirrorscapexr.com)
 
 
